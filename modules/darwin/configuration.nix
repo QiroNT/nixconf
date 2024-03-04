a6ce05017ea1c4e16c974143c54f315db6e3c97d
QiroNT <i@ntz.im>
2024-03-04 08:30:29 +0800

    darwin system stuff

--- modules/darwin/configuration.nix
+++ modules/darwin/configuration.nix
@@ -5,7 +5,40 @@
 
   # used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+  system = {
+    stateVersion = 4;
+
+    defaults = {
+      LaunchServices = {
+        LSQuarantine = false; # disable "this file was downloaded from the internet" warning
+      };
+
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+      };
+
+      CustomUserPreferences = {
+        "com.apple.finder" = {
+          ShowExternalHardDrivesOnDesktop = false;
+          ShowHardDrivesOnDesktop = false;
+          ShowMountedServersOnDesktop = false;
+          ShowRemovableMediaOnDesktop = false;
+        };
+        "com.apple.desktopservices" = {
+          # avoid creating .DS_Store files on network or usb volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false; # why not
+        };
+        "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+        "com.apple.ImageCapture".disableHotPlug = true;
+      };
+    };
+  };
+
+  security.pam.enableSudoTouchIdAuth = true;
 
   nix = {
     gc = {
