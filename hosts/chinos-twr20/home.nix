1b90ab70c54cd3f411f2fee55dedcaf3438baff3
QiroNT <i@ntz.im>
2024-03-10 06:30:16 +0800

    oops

--- hosts/chinos-twr20/home.nix
+++ hosts/chinos-twr20/home.nix
@@ -5,6 +5,6 @@
 
   home.file = {
     "${config.xdg.configHome}/hypr/hyprland.local.conf".source = ./settings/hyprland.local.conf;
-    "${config.xdg.configHome}/hypr/hyprpaper.conf".source = ./settings/hyprland.conf;
+    "${config.xdg.configHome}/hypr/hyprpaper.conf".source = ./settings/hyprpaper.conf;
   };
 }
