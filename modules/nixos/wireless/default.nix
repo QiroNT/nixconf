13f730c35c68d3d90e3ed7ee685485a972a3c9ca
QiroNT <i@ntz.im>
2024-08-03 05:24:34 +0800

    disable powersave to hopefully prevent wifi from dropping

--- modules/nixos/wireless/default.nix
+++ modules/nixos/wireless/default.nix
@@ -15,7 +15,13 @@ in
   config = lib.mkIf cfg.enable {
     networking = {
       wireless.iwd.enable = true;
-      networkmanager.wifi.backend = "iwd";
+      networkmanager.wifi = {
+        backend = "iwd";
+        # Received error during CMD_TRIGGER_SCAN: Operation not supported (95)
+        # Received Deauthentication event, reason: 4, from_ap: false
+        # https://bugzilla.kernel.org/show_bug.cgi?id=203709#c94
+        powersave = false;
+      };
     };
   };
 }
