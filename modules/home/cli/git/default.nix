5b889908bcdbe6e9fb9250425a362afa229a05c6
QiroNT <i@ntz.im>
2024-08-04 03:35:44 +0800

    git sendemail

--- modules/home/cli/git/default.nix
+++ modules/home/cli/git/default.nix
@@ -34,15 +34,28 @@ in
         extraConfig = {
           # why merge when you can stash & rebase
           pull.rebase = "true";
+
           # let git resolve conflicts for you
           rerere.enabled = "true";
+
           # the one git command that have nothing to do with git
           # (this options makes branches display in grid, much better experience)
           column.ui = "auto";
+
           # actually, vscode does this by default and it's much better,
           # should set this in cli too
           branch.sort = "-committerdate";
-          # I still left wondering how on earth would I configure repo maintenance
+
+          # i still left wondering how on earth would i configure repo maintenance
+
+          # tutorial: https://git-send-email.io/
+          # auth with $ git config --global sendemail.smtpPass 'app password'
+          sendemail = {
+            smtpserver = "smtppro.zoho.com";
+            smtpserverport = "587";
+            smtpencryption = "tls";
+            smtpuser = "i@ntz.im";
+          };
         };
       };
 
