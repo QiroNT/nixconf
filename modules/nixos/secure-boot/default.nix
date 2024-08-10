b1f27aac5a300fe4713137665f41645699c95e2e
QiroNT <i@ntz.im>
2024-08-10 18:57:46 +0800

    docs & bump

--- modules/nixos/secure-boot/default.nix
+++ modules/nixos/secure-boot/default.nix
@@ -15,6 +15,7 @@ in
 
   config = lib.mkIf cfg.enable {
     boot = {
+      # secure boot, see lzbt & arch docs for setup
       loader = {
         efi.canTouchEfiVariables = lib.mkForce false;
         systemd-boot.enable = lib.mkForce false;
@@ -23,6 +24,10 @@ in
         enable = true;
         pkiBundle = "/etc/secureboot";
       };
+
+      # to roll disk encryption keys into TPM, use the following:
+      # $ systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7 --wipe-slot=tpm2 /dev/sdX
+      # TODO add pcr 11 after https://github.com/nix-community/lanzaboote/issues/61
       initrd.systemd.enable = true;
     };
   };
