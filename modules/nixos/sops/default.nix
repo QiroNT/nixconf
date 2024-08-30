8cb6f0781b4cdf8478a6b83b7322d0a9c5b04569
QiroNT <i@ntz.im>
2024-08-31 01:51:34 +0800

    move sops key path (because of mounting order)

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -15,7 +15,9 @@ in
   config = lib.mkIf cfg.enable {
     sops = {
       defaultSopsFile = ../../../secrets/common.yaml;
-      age.keyFile = "/home/qiront/.config/sops/age/keys.txt";
+      # $ sudo mkdir -p /var/lib/sops-nix
+      # $ sudo age-keygen -o /var/lib/sops-nix/key.txt
+      age.keyFile = "/var/lib/sops-nix/key.txt";
     };
   };
 }
