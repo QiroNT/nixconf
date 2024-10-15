ae5bd94ed38f55184998c63fcb8ed64aceb0e387
QiroNT <i@ntz.im>
2024-10-16 02:26:26 +1100

    drop iwd

--- modules/nixos/wireless/default.nix
+++ modules/nixos/wireless/default.nix
@@ -14,14 +14,10 @@ in
 
   config = lib.mkIf cfg.enable {
     networking = {
-      wireless.iwd.enable = true;
-      networkmanager.wifi = {
-        backend = "iwd";
-        # Received error during CMD_TRIGGER_SCAN: Operation not supported (95)
-        # Received Deauthentication event, reason: 4, from_ap: false
-        # https://bugzilla.kernel.org/show_bug.cgi?id=203709#c94
-        powersave = false;
-      };
+      # Received error during CMD_TRIGGER_SCAN: Operation not supported (95)
+      # Received Deauthentication event, reason: 4, from_ap: false
+      # https://bugzilla.kernel.org/show_bug.cgi?id=203709#c94
+      networkmanager.wifi.powersave = false;
     };
   };
 }
