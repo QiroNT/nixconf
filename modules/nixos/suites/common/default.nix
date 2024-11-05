45f07b6534072a3b60980392df1528722001e0f3
QiroNT <i@ntz.im>
2024-11-05 22:27:51 +1100

    fail2ban

--- modules/nixos/suites/common/default.nix
+++ modules/nixos/suites/common/default.nix
@@ -76,6 +76,14 @@ in
         # PermitRootLogin = "yes";
       };
     };
+    services.fail2ban = {
+      enable = true;
+      ignoreIP = [
+        "10.0.0.0/8"
+        "172.16.0.0/12"
+        "192.168.0.0/16"
+      ];
+    };
 
     # i dont have a server for wg so...
     services.tailscale.enable = true;
