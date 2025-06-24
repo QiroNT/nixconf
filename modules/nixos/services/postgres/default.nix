cd2aed373a1c13c2ed06400e955ac04329128718
QiroNT <i@chino.dev>
2025-06-24 17:38:13 +1000

    bump postgres

--- modules/nixos/services/postgres/default.nix
+++ modules/nixos/services/postgres/default.nix
@@ -18,7 +18,23 @@ in
       enable = true;
       enableJIT = true;
       enableTCPIP = true;
-      package = pkgs.postgresql_16;
+      package = pkgs.postgresql_17;
+      settings = {
+        max_connections = 200;
+        shared_buffers = "2GB";
+        maintenance_work_mem = "512MB";
+        wal_buffers = "16MB";
+        random_page_cost = 1.1;
+        effective_io_concurrency = 200;
+        work_mem = "10082kB";
+        huge_pages = "off";
+        min_wal_size = "1GB";
+        max_wal_size = "4GB";
+        max_worker_processes = 8;
+        max_parallel_workers = 8;
+        max_parallel_workers_per_gather = 4;
+        max_parallel_maintenance_workers = 4;
+      };
     };
   };
 }
