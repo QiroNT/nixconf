f164128bc80ec3568ee1726a89f7e7813f8b3453
Sayuri Nekomiya <BBH@awsl.rip>
2025-10-20 21:33:19 +1100

    oops

--- flakeModules/modules/users/yuri.nix
+++ flakeModules/modules/users/yuri.nix
@@ -20,7 +20,7 @@
             ++ (lib.optionals (self.lib.hasAny config "docker") [ "docker" ]);
 
             openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiYBPE2ab4jLbcLm/q0kWGfCMoxPxRMR7xi5AwUChLO bbh@awsl.rip",
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiYBPE2ab4jLbcLm/q0kWGfCMoxPxRMR7xi5AwUChLO bbh@awsl.rip"
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINSyGKSSRCE/LCVsdXLfxP+NGliAzCx4iQRJBKx6SGnT bbh@awsl.rip"
             ];
           };
