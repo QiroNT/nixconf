8889a91a80651f22965a0cb2f41bd584dbe99484
Chino Moka <i@chino.dev>
2025-12-31 02:38:07 +1100

    iwd

--- flakeModules/modules/wireless.nix
+++ flakeModules/modules/wireless.nix
@@ -3,11 +3,13 @@
   flake.modules = self.lib.mkAnyNixos "wireless" (
     { ... }:
     {
-      networking = {
+      networking.networkmanager.wifi = {
+        # wpa_supplicant has a 200 BSSID limit (see bss_max_count)
+        backend = "iwd";
         # Received error during CMD_TRIGGER_SCAN: Operation not supported (95)
         # Received Deauthentication event, reason: 4, from_ap: false
         # https://bugzilla.kernel.org/show_bug.cgi?id=203709#c94
-        networkmanager.wifi.powersave = false;
+        powersave = false;
       };
     }
   );
