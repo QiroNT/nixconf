fc4c0b4dbfd19d951d46abad11a07276f4dcbaf2
Chino Moka <i@chino.dev>
2026-04-29 06:30:36 +1000

    patch

--- flakeModules/parts/pkgs.nix
+++ flakeModules/parts/pkgs.nix
@@ -18,6 +18,14 @@ let
       self.overlays.default
       inputs.nur.overlays.default
       inputs.nix-cachyos-kernel.overlays.pinned
+      (_: prev: {
+        # FIXME
+        # https://github.com/NixOS/nixpkgs/issues/426717
+        # https://github.com/NixOS/nixpkgs/issues/513245
+        openldap = prev.openldap.overrideAttrs {
+          doCheck = !prev.stdenv.hostPlatform.isi686;
+        };
+      })
     ];
   };
 in
