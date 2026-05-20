5ab97e76963404829f807677752e620e94e95f56
Chino Moka <i@chino.dev>
2026-05-20 16:47:52 +1000

    key

--- flakeModules/modules/users/yuri/default.nix
+++ flakeModules/modules/users/yuri/default.nix
@@ -18,6 +18,7 @@
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiYBPE2ab4jLbcLm/q0kWGfCMoxPxRMR7xi5AwUChLO bbh@awsl.rip"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINSyGKSSRCE/LCVsdXLfxP+NGliAzCx4iQRJBKx6SGnT bbh@awsl.rip"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPm8fm2GbPerlhMI4jwfjyg3HGIyql/n2XaMNHwHn8nTAAAABHNzaDo= Sayuri Nekomiya's YubiKey 5"
         ];
       };
     }
