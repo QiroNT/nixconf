613f79dac9768b5e326820956b52cef7fa4a9e6d
Chino Moka <i@chino.dev>
2026-06-28 07:34:06 +1000

    bump

--- hosts/x86_64-nixos/chinos-hlb24/caddy.nix
+++ hosts/x86_64-nixos/chinos-hlb24/caddy.nix
@@ -16,7 +16,7 @@ in
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "pkg.para.party/certdx/exec/caddytls@v0.5.0" ];
-      hash = "sha256-FH9ggo5oOSOdu9U+jh/r0HMMnHSRk1rYlQnZxR3IAnw=";
+      hash = "sha256-VqZzlK2mwGeW1simaUuQUGWjRkdWzsrF5/YIRvJyOhU=";
     };
     openFirewall = true;
 
